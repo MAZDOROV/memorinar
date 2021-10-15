159e4b00c745bfe83940ccaa806200075deba827
mazdorov <mazdorov.ilya@mail.ru>
2021-10-15 21:37:48 +0600

    Container deleted

--- src/Container/App.js
+++ src/Container/App.js
@@ -1,8 +1,32 @@
-import Container from "./Container";
+
+import './App.css';
+import Grid from "../Components/Grid"
+import Menu from "../Components/Menu"
+import DataGenerator from "../Store/DataGenerator"
+import PictureGenerator from "../Store/PictureGenegator"
+
+let pictureGenerator = new PictureGenerator();
+let generator = new DataGenerator();
+let frontPictures = pictureGenerator.getFrontPictures();
+let backPictures = pictureGenerator.getBackPictures();
+let gameBoardArr = generator.generate({ rows: 5, columns: 5 }, frontPictures, backPictures)
+
 function App() {
   return (
-    <Container>
-    </Container>
+    <div className="App" >
+      <div className="container pt-3">
+        <div className="row">
+          <div className="col">
+          <Menu></Menu>
+          </div>
+          <div>
+          <header className="App-header">
+            <Grid />
+          </header>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
