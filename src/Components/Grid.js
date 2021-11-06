3f40510405535292f5567235e1e8a9ce21429a28
mazdorov <mazdorov.ilya@mail.ru>
2021-11-07 01:30:53 +0600

    memorinar Попытка выполнить выравнивание игового поля по вертикали. не удачная

--- src/Components/Grid.js
+++ src/Components/Grid.js
@@ -5,23 +5,24 @@ import { connect } from 'react-redux';
 const Grid = ({ gameBord }) => {
 
     return (
-        <div className='board'>
-            {
-                gameBord.map ?
-                    gameBord.map((row, row_index) =>
-                        <div className='board-row' key={row_index}>
-                            {row.map((cell, col_idex) =>
-                                <Card card={cell} key={(row_index.toString() + col_idex.toString())} />
-                            )}
-                        </div>)
-                    : <></>
-            }
+        <div className='vertical-center'>
+            <div className='board'>
+                {
+                    gameBord.map ?
+                        gameBord.map((row, row_index) =>
+                            <div className='board-row' key={row_index}>
+                                {row.map((cell, col_idex) =>
+                                    <Card card={cell} key={(row_index.toString() + col_idex.toString())} />
+                                )}
+                            </div>)
+                        : <></>
+                }
+            </div>
         </div>
     );
 }
 
 const mapStateToProps = state => {
-    console.log(state);
     return {
         gameBord: state.board
     }
